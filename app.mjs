7dd6268bfac46274614422475d5c42a3194a7ba2
Amiqa Saleem <amiqasaleem@gmail.com>
2025-03-18 23:48:07 +0500

    changes made

--- app.mjs
+++ app.mjs
@@ -1,1 +1,15 @@
-console.log("Helloooo Worlddddd");
+import express from "express";
+const app = express();
+
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send('GET request is working')
+})
+app.post('/items',(req,res)=>{
+    res.send('POST Request is working')
+})
+app.listen(3000, ()=>{
+    console.log(`Amiqa is listening on port ${port}`);
+    
+})
